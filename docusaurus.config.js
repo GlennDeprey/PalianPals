4046537edc2e35ff236203598e5574f63c76f2fc
GlennDeprey <95774708+GlennDeprey@users.noreply.github.com>
2025-09-14 11:39:15 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          /**{to: '/blog', label: 'Blog', position: 'left'},**/
           {
             href: `https://discordapp.com/invite/PalianPals`,
             label: 'Discord',
